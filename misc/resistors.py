1500ab940b53f1b179080ffaed18d830534c6017
Conor Gilmer <conor.gilmer@gmail.com>
2023-12-27 11:23:51 +0000

    misc loop

--- misc/resistors.py
+++ misc/resistors.py
@@ -1,15 +1,21 @@
 #Resistor Colour Code calculator
-print("Resistor Colour Code Calculator")
-colour=["black","brown","red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+while True:
+    print("Resistor Colour Code Calculator")
+    colour=["black","brown","red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
 
-n=colour.index((input("Enter the 1st Colour : ")))
-m=colour.index((input("Enter the 2nd Colour : ")))
-p=colour.index((input("Enter the 3rd Colour : ")))
-print("if there is a 4th colour, ignore it for this, it refers to the tolerance")
+    n=colour.index((input("Enter the 1st Colour : ")))
+    m=colour.index((input("Enter the 2nd Colour : ")))
+    p=colour.index((input("Enter the 3rd Colour : ")))
+    print("if there is a 4th colour, ignore it for this, it refers to the tolerance")
 
-q=int(((n*10)+(m))*(10**(p)))
-z=q/1000
+    q=int(((n*10)+(m))*(10**(p)))
+    z=q/1000
 
-print(f"\nThe {colour[n]} {colour[m]} {colour[p]} Resistors resistance is : ")
-print(f"{q} ohms or {z} kiloohms")
+    print(f"\nThe {colour[n]} {colour[m]} {colour[p]} Resistors resistance is : ")
+    print(f"{q} ohms or {z} kiloohms")
 
+    # Prompt the user to continue or quit
+    print('Enter q to quit, or any other key to continue:')
+    choice = input()
+    if choice.lower() == 'q':
+        break
