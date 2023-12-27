4ff9439440acc99a0772aac276314f42492546d5
Conor Gilmer <conor.gilmer@gmail.com>
2023-12-27 11:04:13 +0000

    misc

---
+++ misc/resistors.py
@@ -0,0 +1,15 @@
+#Resistor Colour Code calculator
+print("Resistor Colour Code Calculator")
+colour=["black","brown","red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+
+n=colour.index((input("Enter the 1st Colour : ")))
+m=colour.index((input("Enter the 2nd Colour : ")))
+p=colour.index((input("Enter the 3rd Colour : ")))
+print("if there is a 4th colour, ignore it for this, it refers to the tolerance")
+
+q=int(((n*10)+(m))*(10**(p)))
+z=q/1000
+
+print(f"\nThe {colour[n]} {colour[m]} {colour[p]} Resistors resistance is : ")
+print(f"{q} ohms or {z} kiloohms")
+
